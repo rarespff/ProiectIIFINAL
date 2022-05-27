887fdd88c15b691c7c163c773270b6fa9163eb39
ZETE RARES <rareszete21@yahoo.com>
2022-05-28 00:13:27 +0300

    validation methods

--- EF/Controllers/UsersController.cs
+++ EF/Controllers/UsersController.cs
@@ -9,6 +9,7 @@ using DataAccess.EF.AppDbContext;
 using DataAccess.EF.Models;
 using ProiectII.EF.ViewModels;
 using ProiectII.Services.Interfaces;
+using System.Text.RegularExpressions;
 
 namespace ProiectII.EF.Controllers
 {
@@ -27,7 +28,7 @@ namespace ProiectII.EF.Controllers
 
         [HttpPost]
         [ActionName("Login")]
-        public async Task<ActionResult<User>> Login([FromBody]UserVM userVM)
+        public async Task<ActionResult<User>> Login([FromBody] UserVM userVM)
         {
             //UserVM userReturnVM = new UserVM(await userService.Login(userVM));
             return Ok(await userService.Login(userVM));
@@ -35,7 +36,7 @@ namespace ProiectII.EF.Controllers
 
         [HttpPost]
         [ActionName("Register")]
-        public async Task<ActionResult<String>> RegiserUser([FromBody]AddUserVM addUserVM)
+        public async Task<ActionResult<String>> RegiserUser([FromBody] AddUserVM addUserVM)
         {
             return Ok(await userService.RegiserUser(addUserVM));
         }
@@ -62,7 +63,33 @@ namespace ProiectII.EF.Controllers
         }
 
 
+        #region Validation methods
+        public Boolean CheckFNameLName(String fieldText)
+        {
+            String trimmedText = fieldText.Trim();
+           return Regex.IsMatch(trimmedText, @"^[a-zA-Z]+$");
 
-        
+        }
+        public Boolean CheckUsername(String userName)
+        {
+            String trimmedName = userName.Trim();
+            Char firstLetter = trimmedName[0];
+            return (Regex.IsMatch(trimmedName, @"^[a-zA-Z0-9]+$")) && (Char.IsLetter(firstLetter));
+
+        }
+        private Boolean CheckEmail(String email)
+        {
+            String trimmedEmail = email.Trim();
+            Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            Match match = regex.Match(email);
+            return match.Success;
+
+        }
+        #endregion
+
+
+
+
+
     }
 }
